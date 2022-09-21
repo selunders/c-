722c6cf48f6e8baa4409442127e5fb717e8c254d
selunders <sethlunders@gmail.com>
2022-09-21 10:09:45 -0700

    test.sh now compares against all files in sample folder

--- tests.sh
+++ tests.sh
@@ -1,24 +1,32 @@
 #!/bin/bash
 
-# echo "Assignment 1 Files:"
-# for file in samplePrograms/*.c-
-# do
-#    echo ./c- $file;
-#    ./c- $file
-#done
-#
-#echo ""
-echo "Assignment 2 files:"
-for file in samplePrograms2/*.c-
-do
-    echo ./c- $file
-    ./c- $file
-done
-#
-#echo ""
-#echo "Assignment 2 Error Files:"
-#for file in samplePrograms2_Errors/*.c-
-#do
-#    echo ./c- $file
-#    ./c- $file
-#done
+
+
+if [ -z $1 ]; then
+    echo 'make c-'
+    make clean; make;
+    echo -e "\n"
+    echo "Assignment 2 files:"
+    for file in samplePrograms2/*.c-
+    do
+        x=${file%.c-}
+        y=${x##*/}
+        echo 'Running ./c- on all the files in samplePrograms2'
+        # echo "./c- $file > myOutput/$y.out" 
+        ./c- $file > myOutput/$y.out
+    done
+
+    echo -e "Complete. Files stored in myOutput/\n"
+
+    echo 'Diffing outputs'
+    for file in samplePrograms2/*.out
+    do
+        x=${file%.out}
+        y=${x##*/}
+        echo -e "\nsdiff -l -s myOutput/$y.out $file"
+        sdiff -l -s myOutput/$y.out $file
+    done
+else
+    echo "sdiff myOutput/$1.out samplePrograms2/$1.out"
+    sdiff myOutput/$1.out samplePrograms2/$1.out
+fi
